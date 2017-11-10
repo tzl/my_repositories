89edddea4657b220143f5416e3926ca529e2977a
tom <tzl2000@gmail.com>
2017-11-10 19:44:11 +0800

    pattern match test

--- java/test/src/src/main/RegexTest.java
+++ java/test/src/src/main/RegexTest.java
@@ -9,10 +9,7 @@ import java.util.regex.Pattern;
 
 public class RegexTest
 {
-    final static Pattern sUppercasePat = Pattern.compile("((\\d|[A-F]){32}).*");
-    final static Pattern sLowercasePat = Pattern.compile("((\\d|[a-f]){32}).*");
-    final static Pattern sCuidUppercasePat = Pattern.compile("((\\d|[A-F]){32}).*(\\|.*)");
-    final static Pattern sCuidLowercasePat = Pattern.compile("((\\d|[a-f]){32}).*(\\|.*)");
+    private static Pattern sNonCashBindPat = Pattern.compile("(3|1(1|2))");
 
     public static void main(String[] args)
     {
@@ -23,27 +20,9 @@ public class RegexTest
 
         System.out.println("input:\n\t" + args[0]);
 
-        Matcher matcher = sUppercasePat.matcher(args[0]);
-        String validStr = matcher.matches()? matcher.group(1): null;
-        if (null == validStr) {
-            matcher = sLowercasePat.matcher(args[0]);
-            validStr = matcher.matches()? matcher.group(1): "no match things";
-        }
+        Matcher matcher = sNonCashBindPat.matcher(args[0]);
+        String validStr = matcher.matches()? matcher.group(): null;
 
-        matcher = sCuidUppercasePat.matcher(args[0]);
-        int count = matcher.groupCount();
-        if (matcher.matches()) {
-            System.out.println("count=" + count);
-            for (int i = 0; i <= count; ++i) {
-                System.out.println("group[" + i + "]=" + matcher.group(i));
-            }
-        }
-
-        String imei = matcher.matches()? matcher.group(3): null;
-        if (null == imei) {
-            matcher = sCuidLowercasePat.matcher(args[0]);
-            imei = matcher.matches()? matcher.group(3): "no match things";
-        }
-        System.out.println("deviceid:\n\t" + validStr + "\nimei:\n\t" + imei);
+        System.out.println("match data:\n\t" + validStr);
     }
 }
